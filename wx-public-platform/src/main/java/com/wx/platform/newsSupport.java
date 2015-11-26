006f7a445731ce9beac20ecaf035bd7da69beb0c
justcby <just.cby@163.com>
2015-11-26 21:15:11 +0800

    upload picture

--- wx-public-platform/src/main/java/com/wx/platform/newsSupport.java
+++ wx-public-platform/src/main/java/com/wx/platform/newsSupport.java
@@ -1,11 +1,9 @@
 package com.wx.platform;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
 
 import com.wx.platform.api.MaterialAPI;
 import com.wx.platform.api.config.ApiConfig;
-import com.wx.platform.api.entity.Article;
 import com.wx.platform.api.response.UploadMaterialResponse;
 import com.wx.platform.config.CommonConfig;
 
@@ -15,25 +13,28 @@ public class newsSupport {
 	private static final String SECRET = CommonConfig.SECRET;
 
 	public static void main(String[] args) {
-		
-		
-		
-		
 		ApiConfig apiCon = new ApiConfig(APPID, SECRET);
 
 		MaterialAPI mp = new MaterialAPI(apiCon);
-		Article article = new Article();
-		article.setAuthor("Jason");
-		article.setContent("Testing");
-		article.setContentSourceUrl("localhost:8080");
-		article.setDigest("Welcome to new fellows");
-		article.setShowConverPic(0);
-		article.setTitle("New fellows");
-		//article.setThumbMediaId("1111");
-		List<Article> art = new ArrayList<Article>();
-		art.add(article);
-		UploadMaterialResponse ur = mp.uploadMaterialNews((List<Article>) art);
-		System.out.println(ur.getMediaId());
+		
+		
+		String pathname = "D:\\example.jpg";
+		File file = new File(pathname);
+		UploadMaterialResponse ur1 = mp.uploadMaterialFile(file);
+		System.out.println(ur1.getMediaId());
+		
+//		Article article = new Article();
+//		article.setAuthor("Jason");
+//		article.setContent("Testing");
+//		article.setContentSourceUrl("localhost:8080");
+//		article.setDigest("Welcome to new fellows");
+//		article.setShowConverPic(0);
+//		article.setTitle("New fellows");
+//		//article.setThumbMediaId("1111");
+//		List<Article> art = new ArrayList<Article>();
+//		art.add(article);
+//		UploadMaterialResponse ur = mp.uploadMaterialNews((List<Article>) art);
+//		System.out.println(ur.getMediaId());
 		
 	}
 
